fd8aebd4f11988eddf3e2d3a75d058cd7bb29a7b
sanchq2 <semin.alex222@yandex.ru>
2024-01-23 16:07:38 +0300

    create UserDataResponse

---
+++ client-app/src/main/java/com/projectsav/client_app/messages/users/GetUserDataResponse.java
@@ -0,0 +1,13 @@
+package com.projectsav.client_app.messages.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetUserDataResponse {
+}
