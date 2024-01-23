50e9821b30b6a6e506f531305a34c581841abdd2
sanecheqq <semin.alex222@yandex.ru>
2024-01-23 18:18:49 +0300

    code fixes

--- client-app/src/main/java/com/projectsav/client_app/controllers/UserController.java
+++ client-app/src/main/java/com/projectsav/client_app/controllers/UserController.java
@@ -1,20 +1,25 @@
 package com.projectsav.client_app.controllers;
 
-import com.projectsav.client_app.messages.users.GetUserDataResponse;
+import com.projectsav.client_app.messages.dtos.UserDto;
+import com.projectsav.client_app.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
     @GetMapping("/{user_id}")
-    public ResponseEntity<GetUserDataResponse> getUserData(
+    public ResponseEntity<UserDto> getUserData(
             @PathVariable(name = "user_id") String userId
     ) {
-
-//        return ResponseEntity.status(HttpStatus.OK).body();
-        return null;
+        UserDto userData = userService.getUserData(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(userData);
     }
 
 }
