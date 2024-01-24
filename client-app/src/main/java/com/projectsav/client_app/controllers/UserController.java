d96c3b78858ab15330c543908ee10a3ce463e6fc
sanecheqq <semin.alex222@yandex.ru>
2024-01-24 20:46:53 +0300

    add endpoint for getting user data by jwt

--- client-app/src/main/java/com/projectsav/client_app/controllers/UserController.java
+++ client-app/src/main/java/com/projectsav/client_app/controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.projectsav.client_app.controllers;
 
 import com.projectsav.client_app.messages.dtos.UserDto;
+import com.projectsav.client_app.security.CustomUserDetails;
 import com.projectsav.client_app.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/{user_login}")
+    @GetMapping("/user/{user_login}")
     public ResponseEntity<UserDto> getUserData(
             @PathVariable(name = "user_login") String login
     ) {
         UserDto userData = userService.getUserData(login);
         return ResponseEntity.status(HttpStatus.OK).body(userData);
+    }
+
+    @GetMapping("/user")
+    public String getUserByToken(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        return userDetails.getUsername();
     }
 
 }
