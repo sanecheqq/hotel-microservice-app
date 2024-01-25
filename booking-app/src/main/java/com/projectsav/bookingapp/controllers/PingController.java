f8cc80cf16422e65302b629c8fa1cc236a3afacc
sanecheqq <semin.alex222@yandex.ru>
2024-01-25 14:03:41 +0300

    fixes

--- booking-app/src/main/java/com/projectsav/bookingapp/controllers/PingController.java
+++ booking-app/src/main/java/com/projectsav/bookingapp/controllers/PingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
     @GetMapping("/ping")
-    public String openPing() {
+    public String ping() {
         return "pong";
     }
 
