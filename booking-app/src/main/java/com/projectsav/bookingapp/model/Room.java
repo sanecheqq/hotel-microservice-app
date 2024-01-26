c46dd98c2b76e5a8aab3354a2174f289111deab6
sanecheqq <semin.alex222@yandex.ru>
2024-01-26 15:18:02 +0300

    remove generationType, because the room numbers are unique and special - it must be set by hands but not auto generated

--- booking-app/src/main/java/com/projectsav/bookingapp/model/Room.java
+++ booking-app/src/main/java/com/projectsav/bookingapp/model/Room.java
@@ -1,6 +1,7 @@
 package com.projectsav.bookingapp.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -8,11 +9,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "hotel_rooms")
 public class Room {
     @Id
-    @GeneratedValue
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long number; // room number - unique value
 
     @Column(name = "price")
