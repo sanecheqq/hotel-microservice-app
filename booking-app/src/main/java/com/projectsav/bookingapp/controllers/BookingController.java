d878d5dc95ea731d39ae81a54ab4cc94d77cde08
sanecheqq <semin.alex222@yandex.ru>
2024-01-26 15:19:23 +0300

    add auth:ping endpoint for local testing and end for half booking endpoint

--- booking-app/src/main/java/com/projectsav/bookingapp/controllers/BookingController.java
+++ booking-app/src/main/java/com/projectsav/bookingapp/controllers/BookingController.java
@@ -1,26 +1,31 @@
 package com.projectsav.bookingapp.controllers;
 
 import com.projectsav.bookingapp.messages.dtos.BookingDto;
+import com.projectsav.bookingapp.messages.dtos.UserDto;
+import com.projectsav.bookingapp.services.AuthService;
 import com.projectsav.bookingapp.services.BookingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/booking")
 @RequiredArgsConstructor
 public class BookingController {
+    private final AuthService authService;
     private final BookingService bookingService;
     @PostMapping("/{room_number}")
     public ResponseEntity<BookingDto> bookRoom(
             @PathVariable(name = "room_number") Long roomNumber,
-            @RequestParam(name = "start", defaultValue = "01/01/24 00:00:00") Timestamp startDate,
-            @RequestParam(name = "end", defaultValue = "01/01/24 00:00:00") Timestamp endDate,
+            @RequestParam(name = "start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam(name = "end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
             @RequestHeader("Authorization") String authHeader
     ) {
+        UserDto userData = authService.getUserData(authHeader);
         return ResponseEntity.status(HttpStatus.OK).body(new BookingDto());
     }
 }
--- booking-app/src/main/java/com/projectsav/bookingapp/controllers/PingController.java
+++ booking-app/src/main/java/com/projectsav/bookingapp/controllers/PingController.java
@@ -1,13 +1,29 @@
 package com.projectsav.bookingapp.controllers;
 
+import com.projectsav.bookingapp.messages.dtos.UserDto;
+import com.projectsav.bookingapp.services.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PingController {
+    private final AuthService authService;
+
     @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @GetMapping("/auth:ping")
+    public String authPing(
+            @RequestHeader("Authorization") String authHeader
+    ) {
+        UserDto userData = authService.getUserData(authHeader);
+        System.out.println(userData);
+        return "auth-pong";
     }
 
 }
