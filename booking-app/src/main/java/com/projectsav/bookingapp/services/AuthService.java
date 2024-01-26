0310a75e011061d6eb537519539cae1946b4cbda
sanecheqq <semin.alex222@yandex.ru>
2024-01-26 15:16:37 +0300

    create method to get userData from client-app and check authorization by jwt

--- booking-app/src/main/java/com/projectsav/bookingapp/services/AuthService.java
+++ booking-app/src/main/java/com/projectsav/bookingapp/services/AuthService.java
@@ -3,5 +3,5 @@ package com.projectsav.bookingapp.services;
 import com.projectsav.bookingapp.messages.dtos.UserDto;
 
 public interface AuthService {
-    UserDto getUserData(String jwt);
+    UserDto getUserData(String authHeader);
 }
--- booking-app/src/main/java/com/projectsav/bookingapp/services/AuthServiceImpl.java
+++ booking-app/src/main/java/com/projectsav/bookingapp/services/AuthServiceImpl.java
@@ -1,10 +1,17 @@
 package com.projectsav.bookingapp.services;
 
+import com.projectsav.bookingapp.aop.HeaderRequestInterceptor;
 import com.projectsav.bookingapp.messages.dtos.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +21,22 @@ public class AuthServiceImpl implements AuthService {
     private final DiscoveryClient discoveryClient;
 
     @Override
-    public UserDto getUserData(String jwt) {
-        //TODO: realize sending request to client-app with bearer token and get userDto.
-        return null;
+    public UserDto getUserData(String authHeader) {
+        ServiceInstance backendInstance = discoveryClient.getInstances("client-backend").get(0);
+        URI getUserByTokenUri = backendInstance.getUri().resolve("/users/user");
+
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new HeaderRequestInterceptor("Authorization", authHeader));
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate.getForObject(getUserByTokenUri, UserDto.class);
     }
+
+    private String parseAuthHeader(String authHeader) {
+        if (authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return "";
+    }
+
+
 }
