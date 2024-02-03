ad9c83009272a919610d5966990f4356534c92aa
sanecheqq <semin.alex222@yandex.ru>
2024-02-03 16:59:47 +0300

    some upgrades of jpa query

--- booking-app/src/main/java/com/projectsav/bookingapp/repositories/BookingRepository.java
+++ booking-app/src/main/java/com/projectsav/bookingapp/repositories/BookingRepository.java
@@ -3,19 +3,29 @@ package com.projectsav.bookingapp.repositories;
 import com.projectsav.bookingapp.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.sql.Timestamp;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, String> {
-    @Query("SELECT b FROM Booking b WHERE b.room.number = ?1 " +
-            "and ((b.start <= ?2 and b.end >= ?2) or (b.start <= ?3 and b.end >= ?3) or (?2 <= b.start and b.end <= ?3))")
     /*
-         1. (b.start <= ?2 and b.end >= ?2) - case when incoming 'start' between existing booking dates - b.start and b.end
-         2. (b.start <= ?3 and b.end >= ?3) - case when incoming 'end' between existing booking dates - b.start and b.end
-         3. (?2 <= b.start and b.end <= ?3) - case when existing booking dates are between incoming start and end:
-         visually: [   (     )     ] - ?2 - Timestamp start, ?3 - Timestamp end, b.s and b.e - b.start and b.end
-                  ?2   b.s   b.e  ?3
+         1-2. - simple cases when provided start or end get between b.start and b.end;
+         3. (:start <= b.start and b.end <= :end) - case when existing booking dates are between incoming start and end:
+         visually: [      (       )       ] - b.s and b.e are b.start and b.end
+                 :start   b.s    b.e    :end
      */
-    List<Booking> getRoomsWithIntersectingDate(Long roomNumber, Timestamp start, Timestamp end);
+    @Query("SELECT b FROM Booking b WHERE " +
+            "b.room.number = :roomNum " +
+            "AND (" +
+                "   (:start BETWEEN b.start AND b.end) " +
+                "OR (:end BETWEEN b.start AND b.end) " +
+                "OR (:start <= b.start AND b.end <= :end)" +
+            ")"
+    )
+    List<Booking> getRoomsWithIntersectingDate(
+            @Param("roomNum") Long roomNumber,
+            @Param("start") Timestamp start,
+            @Param("end") Timestamp end
+    );
 }
