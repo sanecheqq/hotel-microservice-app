f855c3e5b1e0664eb56c8b515d3ea54210d8c5c7
sanecheqq <semin.alex222@yandex.ru>
2024-02-05 12:26:14 +0300

    fixing queries again

--- booking-app/src/main/java/com/projectsav/bookingapp/repositories/RoomRepository.java
+++ booking-app/src/main/java/com/projectsav/bookingapp/repositories/RoomRepository.java
@@ -12,44 +12,60 @@ import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     /*
-        Find all available rooms which can be booked on provided start and end dates (from start to end).
+        Find all available rooms which can be booked on provided start and end dates (from start to end),
+        also the type and max-min prices parameters are taken into account.
         Verification condition are the same as in the BookingRepository, but also we need to make left join with booking table
         to check all bookings for current room.
      */
+
     @Query(value = """
         SELECT DISTINCT r FROM Room r LEFT JOIN r.bookings
-        WHERE NOT EXISTS (
-            SELECT 1 FROM Booking b
-            WHERE b.room = r
-                AND (
-                    (:start BETWEEN b.start AND b.end)
-                    OR (:end BETWEEN b.start AND b.end)
-                    OR (:start <= b.start AND b.end <= :end)
-                )
-        )
+        WHERE (:room_type IS NULL OR :room_type = r.type)
+            AND (:max_price IS NULL OR r.price <= :max_price)
+            AND (:min_price IS NULL OR r.price >= :min_price)
+            AND NOT EXISTS (
+                SELECT 1 FROM Booking b
+                WHERE b.room = r
+                    AND (
+                        (:start BETWEEN b.start AND b.end)
+                        OR (:end BETWEEN b.start AND b.end)
+                        OR (:start <= b.start AND b.end <= :end)
+                    )
+            )
     """)
-    List<Room> getAvailableRoomsByStartAndEndDatesAndSort(
+    List<Room> getAvailableRoomsByRoomTypeAndMaxPriceAndMinPriceAndStartAndEndDatesAndSort(
+            @Param("room_type") RoomType roomType,
+            @Param("max_price") Float maxPrice,
+            @Param("min_price") Float minPrice,
             @Param("start") Timestamp start,
             @Param("end") Timestamp end,
             Sort sort
     );
 
+    /*
+        WITHOUT SORT
+     */
+    @Query(value = """
+        SELECT DISTINCT r FROM Room r LEFT JOIN r.bookings
+        WHERE (:room_type IS NULL OR :room_type = r.type)
+            AND (:max_price IS NULL OR r.price <= :max_price)
+            AND (:min_price IS NULL OR r.price >= :min_price)
+            AND NOT EXISTS (
+                SELECT 1 FROM Booking b
+                WHERE b.room = r
+                    AND (
+                        (:start BETWEEN b.start AND b.end)
+                        OR (:end BETWEEN b.start AND b.end)
+                        OR (:start <= b.start AND b.end <= :end)
+                    )
+            )
+    """)
 
-    @Query("SELECT DISTINCT r FROM Room r LEFT JOIN r.bookings WHERE " +
-            "r.type = :room_type " +
-            "AND NOT EXISTS (" +
-                "SELECT 1 FROM Booking b WHERE " +
-                "b.room = r AND (" +
-                    "   (:start BETWEEN b.start AND b.end) " +
-                    "OR (:end BETWEEN b.start AND b.end) " +
-                    "OR (:start <= b.start AND b.end <= :end)" +
-                ")" +
-            ")"
-    )
-    List<Room> getAvailableRoomsByStartAndEndDatesAndRoomTypeAndSort(
+    List<Room> getAvailableRoomsByRoomTypeAndMaxPriceAndMinPriceAndStartAndEndDates(
+            @Param("room_type") RoomType roomType,
+            @Param("max_price") Float maxPrice,
+            @Param("min_price") Float minPrice,
             @Param("start") Timestamp start,
-            @Param("end") Timestamp end,
-            @Param("room_type") RoomType room_type,
-            Sort sort
+            @Param("end") Timestamp end
     );
 }
